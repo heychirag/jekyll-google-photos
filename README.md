7bb3efe7b94b019eb3657d6b89cd3f31207a393f
heychirag <i@chira.ga>
2019-08-18 21:15:37 +0530

    Amazing CSS Grid: flexbin

--- README.md
+++ README.md
@@ -9,7 +9,6 @@
 add the following to your Gemfile:
 ```
 gem 'jekyll-google-photos'
-gem 'fastimage'
 ```
 
 then:
@@ -26,16 +25,21 @@ plugins:
 
 # Usage:
 ```
-{% google_photos <Link to Shared Google Photos Album> %}
+{% google_photos <link_to_album> row_height row_height_tablet row_height_phone tablet_max phone_max margin %}
 ```
+`row_height`: Row height on the biggest screen (in pixels)
+`row_height_tablet`: Row height on tablets (in pixels)
+`row_height_phone`: Row height on phones (in pixels)
+`tablet_max`: Width below which `row_height_tablet` will be used (in pixels)
+`phone_max`: Width below which `row_height_phone` will be used (in pixels)
+`margin`: space or margin between rows and adjacent photos
+
 Example:
 ```
-{% google_photos https://photos.app.goo.gl/bhWukds8QVodFU246 %}
+{% google_photos https://photos.app.goo.gl/bhWukds8QVodFU246 300 150 100 1280 400 5 %}
 ```
 
 # Features to implement
 
-* Revamp Grid Layout Algorithm
-* Let users decide how many photos in each row
 * Full-Screen HQ Image View and Slider
 * Do something about Videos
--- lib/jekyll-google-photos/tag.rb
+++ lib/jekyll-google-photos/tag.rb
@@ -6,15 +6,20 @@ require 'net/http'
 
 module JekyllGooglePhotos
   class Tag < Liquid::Tag
-    def initialize(tagName, url, tokens)
+    def initialize(tagName, args, tokens)
       super
-      @small = 400
-      @medium = 800
-      @large = 1400
-      @tolerance = 200
+      args = args.split(" ")
+      url = args[0]
+      @row_height = args[1].to_i
+      @space = args[6].to_i
+      @tablet_max = args[4].to_i
+      @row_height_tablet = args[2].to_i
+      @space_tablet = @space
+      @phone_max = args[5].to_i
+      @row_height_phone = args[3].to_i
+      @space_phone = @space
       @imgLinks = getImageLinks(url)
-      @dom = createPSWPElem()
-      @dom += addScript()
+      @dom = createDOM()
     end
 
     def getImageLinks(url)
@@ -31,170 +36,128 @@ module JekyllGooglePhotos
       return json[1]
     end
 
-    def createPSWPElem()
-      elem = %Q{<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-      <!-- Background of PhotoSwipe.
-           It's a separate element as animating opacity is faster than rgba(). -->
-      <div class="pswp__bg"></div>
-      <!-- Slides wrapper with overflow:hidden. -->
-      <div class="pswp__scroll-wrap">
-          <!-- Container that holds slides.
-              PhotoSwipe keeps only 3 of them in the DOM to save memory.
-              Don't modify these 3 pswp__item elements, data is added later on. -->
-          <div class="pswp__container">
-              <div class="pswp__item"></div>
-              <div class="pswp__item"></div>
-              <div class="pswp__item"></div>
-          </div>
-          <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
-          <div class="pswp__ui pswp__ui--hidden">
-              <div class="pswp__top-bar">
-                  <!--  Controls are self-explanatory. Order can be changed. -->
-                  <div class="pswp__counter"></div>
-                  <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-                  <button class="pswp__button pswp__button--share" title="Share"></button>
-                  <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-                  <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-                  <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
-                  <!-- element will get class pswp__preloader--active when preloader is running -->
-                  <div class="pswp__preloader">
-                      <div class="pswp__preloader__icn">
-                        <div class="pswp__preloader__cut">
-                          <div class="pswp__preloader__donut"></div>
-                        </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                  <div class="pswp__share-tooltip"></div>
-              </div>
-              <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
-              </button>
-              <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
-              </button>
-              <div class="pswp__caption">
-                  <div class="pswp__caption__center"></div>
-              </div>
-          </div>
-      </div>
-  </div>}
+    def flexbinCSS()
+      elem = %Q{
+                  .flexbin {
+                       display: flex;
+                       overflow: hidden;
+                       flex-wrap: wrap;
+                       margin: -#{@space/2.0}px;
+                  }
+                   .flexbin:after {
+                       content: '';
+                       flex-grow: 999999999;
+                       min-width: #{@row_height}px;
+                       height: 0;
+                  }
+                   .flexbin > * {
+                       position: relative;
+                       display: block;
+                       height: #{@row_height}px;
+                       margin: #{@space/2.0}px;
+                       flex-grow: 1;
+                  }
+                   .flexbin > * > img {
+                       height: #{@row_height}px;
+                       object-fit: cover;
+                       max-width: 100%;
+                       min-width: 100%;
+                       vertical-align: bottom;
+                  }
+                   .flexbin.flexbin-margin {
+                       margin: #{@space/2.0}px;
+                  }
+                   @media (max-width: #{@tablet_max}px) {
+                       .flexbin {
+                           display: flex;
+                           overflow: hidden;
+                           flex-wrap: wrap;
+                           margin: -#{@space/2.0}px;
+                      }
+                       .flexbin:after {
+                           content: '';
+                           flex-grow: 999999999;
+                           min-width: #{@row_height_tablet}px;
+                           height: 0;
+                      }
+                       .flexbin > * {
+                           position: relative;
+                           display: block;
+                           height: #{@row_height_tablet}px;
+                           margin: #{@space/2.0}px;
+                           flex-grow: 1;
+                      }
+                       .flexbin > * > img {
+                           height: #{@row_height_tablet}px;
+                           object-fit: cover;
+                           max-width: 100%;
+                           min-width: 100%;
+                           vertical-align: bottom;
+                      }
+                       .flexbin.flexbin-margin {
+                           margin: #{@space/2.0}px;
+                      }
+                  }
+                   @media (max-width: #{@phone_max}px) {
+                       .flexbin {
+                           display: flex;
+                           overflow: hidden;
+                           flex-wrap: wrap;
+                           margin: -#{@space/2.0}px;
+                      }
+                       .flexbin:after {
+                           content: '';
+                           flex-grow: 999999999;
+                           min-width: #{@row_height_phone}px;
+                           height: 0;
+                      }
+                       .flexbin > * {
+                           position: relative;
+                           display: block;
+                           height: #{@row_height_phone}px;
+                           margin: #{@space/2.0}px;
+                           flex-grow: 1;
+                      }
+                       .flexbin > * > img {
+                           height: #{@row_height_phone}px;
+                           object-fit: cover;
+                           max-width: 100%;
+                           min-width: 100%;
+                           vertical-align: bottom;
+                      }
+                       .flexbin.flexbin-margin {
+                           margin: #{@space/2.0}px;
+                      }
+                  }
+
+      }
       return elem
     end
 
-    def addScript()
-      sp = %Q{<script>}
-      sp += %Q{var pswpElement = document.querySelectorAll('.pswp')[0];}
+    def createDOM()
+      sp = %Q{<style>}
+      sp += flexbinCSS()
+      sp += %Q{</style>}
       sp += addImages()
-      sp += addOptions()
-      sp += %Q{var GPGallery = new PhotoSwipe(pswpElement,
-                                              PhotoSwipeUI_Default,
-                                              GPImages, GPOptions);}
-      sp += responsiveImages()
-      sp += %Q{GPGallery.init()}
-      sp += %Q{</script>}
-      return sp
-    end
-
-    def addOptions()
-      sp = %Q{var GPOptions = [}
-      sp += %Q{];}
       return sp
     end
 
     def addImages()
-      sp = %Q{var GPImages = [}
+      sp = %Q{<div class="flexbin">}
       for x in @imgLinks
-        smallLink = x[1][0] + %Q{=w#{@small}}
-        smallDims = FastImage.size(smallLink);
-        medLink = x[1][0] + %Q{=w#{@medium}}
-        medDims = FastImage.size(medLink)
-        larLink = x[1][0] + %Q{=w#{@large}}
-        larDims = FastImage.size(larLink)
-        puts smallLink
-        puts medLink
-        puts larLink
-        sp += %Q{\{
-                    small: \{
-                      src: '#{smallLink}',
-                      w: #{smallDims[0]},
-                      h: #{smallDims[1]}
-                    \},
-                    medium: \{
-                      src: '#{medLink}',
-                      w: #{medDims[0]},
-                      h: #{medDims[1]}
-                    \},
-                    large: \{
-                      src: '#{larLink}',
-                      w: #{larDims[0]},
-                      h: #{larDims[1]}
-                    \}
-                  \},}
+        link = x[1][0] + %Q{=w#{@tablet_max}}
+        sp += %Q{
+                  <a href="#{x[1][0]}">
+                      <img src="#{link}" />
+                  </a>
+        }
       end
-      sp += %Q{];}
-      return sp
-    end
-
-    def responsiveImages()
-      sp = %Q{
-      var realViewportWidth,
-      useLarge = false,
-      useMedium = false,
-      firstResize = true,
-      imageSrcWillChange;
-      GPGallery.listen('beforeResize', function() \{
-        realViewportWidth = GPGallery.viewportSize.x * window.devicePixelRatio;
-        if(useLarge && !useMedium &&realViewportWidth >= #{@small+@tolerance}) \{
-          useLarge = false;
-          useMedium = true;
-          imageSrcWillChange = true;
-        \}
-        else if(!useLarge && useMedium && realViewportWidth >= #{@medium+@tolerance}) \{
-          useLarge = true;
-          useMedium = false;
-          imageSrcWillChange = true;
-        \}
-        else \{
-          useMedium = false;
-          useLarge = false;
-          imageSrcWillChange = true;
-        \}
-
-      if(imageSrcWillChange && !firstResize) \{
-        gallery.invalidateCurrItems();
-      \}
-
-      if(firstResize) \{
-        firstResize = false;
-      \}
-
-      imageSrcWillChange = false;
-
-      \});
-
-      GPGallery.listen('gettingData', function(index, item) \{
-        if(useLarge) \{
-          item.src = item.large.src;
-          item.w = item.large.w;
-          item.h = item.large.h;
-        \}
-        else if (useMedium) \{
-          item.src = item.medium.src;
-          item.w = item.medium.w;
-          item.h = item.medium.h;
-        \}
-        else \{
-          item.src = item.small.src;
-          item.w = item.small.w;
-          item.h = item.small.h;
-        \}
-      \});
-      }
+      sp += %Q{</div>}
       return sp
     end
 
     def render(context)
-      "#{@dom}"
+      @dom
     end
   end
 end
